93c96e3a839a7d9ecea632ea02aa55a1a93b0699
Javier Eguiluz <javier.eguiluz@gmail.com>
2026-05-27 08:18:00 +0200

    Prepare 5.0.10 release

--- src/EasyAdminBundle.php
+++ src/EasyAdminBundle.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class EasyAdminBundle extends Bundle
 {
-    public const VERSION = '5.0.10-DEV';
+    public const VERSION = '5.0.10';
 
     public function getPath(): string
     {
