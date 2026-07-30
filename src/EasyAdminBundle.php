7d923d0a51716c4f9e0d8cada0de54de1a1294bd
Javier Eguiluz <javier.eguiluz@gmail.com>
2026-07-30 21:22:20 +0200

    Bump development version

--- src/EasyAdminBundle.php
+++ src/EasyAdminBundle.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class EasyAdminBundle extends Bundle
 {
-    public const VERSION = '5.4.1';
+    public const VERSION = '5.4.2-DEV';
 
     public function getPath(): string
     {
